8f540ce19b04cda072bb5dd30f802c7d6112492c
Miguel Mota <miguelmota2@gmail.com>
2019-04-26 02:26:19 -0700

    always enabled

--- extension/background.js
+++ extension/background.js
@@ -1,26 +1,27 @@
-(function() {
-  //let active = !!(localStorage.getItem("the_finger_enabled") == "true");
-  let active = true;
+chrome.tabs.onUpdated.addListener(callback)
+chrome.tabs.onActivated.addListener(callback)
+chrome.tabs.onCreated.addListener(callback)
 
-  chrome.browserAction.onClicked.addListener(function(tab) {
-    active = !active;
-    //localStorage.setItem("the_finger_enabled", active);
-
-    chrome.tabs.query({}, function(tabs) {
-      tabs.forEach(function(tab) {
-        if (!/^http/gi.test(tab.url)) {
-          return false;
+chrome.runtime.onInstalled.addListener(function (details, tab) {
+  if (details.reason == 'install') {
+    chrome.tabs.query({}, function (tabs) {
+      for (var i = 0; i < tabs.length; i++) {
+        if (tabs && tabs[i].url.indexOf('http') === 0) {
+          chrome.tabs.executeScript(tabs[i].id, { code: 'var link = document.createElement("link");link.href = chrome.extension.getURL("main.css");link.type = "text/css"; link.rel = "stylesheet"; document.getElementsByTagName("head")[0].appendChild(link);document.body.classList.add("fingerCursor")' })
         }
+      }
+    })
+  }
+})
 
-        chrome.browserAction.setIcon({
-          path: active ? "on.png" : "icon.png",
-          tabId: tab.id
-        });
-
+function callback () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    tabs.forEach(function (tab) {
+      if (tab && tab.url.indexOf('http') === 0) {
         chrome.tabs.executeScript(tab.id, {
-          code: "window.thefinger.toggle(" + active + ")"
-        });
-      });
-    });
-  });
-})();
+          code: 'document.body.classList.add("fingerCursor");'
+        })
+      }
+    })
+  })
+}
